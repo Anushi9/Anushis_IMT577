5908e41e9e0322c3769dec17633be947f81a4f45
Anushi9 <102567129+Anushi9@users.noreply.github.com>
2022-05-15 18:02:49 -0700

    Update SQL_Module7.sql

--- module7/SQL_Module7.sql
+++ module7/SQL_Module7.sql
@@ -1,21 +1,20 @@
 USE IMT577_DW_ANUSHISETH_MODULE5
 
 /*
+ * Fact_ProductSalesTarget  
+ */
 
-*/
+-- DROP AND CREATE TABLE Fact_ProductSalesTarget
 
+DROP TABLE Fact_ProductSalesTarget;
 
-Drop table Fact_ProductSalesTarget;
-
-Create Table Fact_ProductSalesTarget (
+CREATE TABLE Fact_ProductSalesTarget (
   DimProductID INTEGER CONSTRAINT FK_DimProductID FOREIGN KEY REFERENCES Dim_Product(ProductID), --Foreign Key
   DimTargetDateID Number (9) CONSTRAINT FK_TargetDateID FOREIGN KEY REFERENCES Dim_Date(Date_Pkey), --Foreign Key
   ProductTargetSalesQuantity INT NOT NULL
 );
 
-
-//INSERT INTO Fact_ProductSalesTarget (DimProductID, DimTargetDateID,ProductTargetSalesQuantity)
-//Values (-1,-1,-1,-1)
+-- INSERT DATA INTO TABLE Fact_ProductSalesTarget
 
 INSERT INTO Fact_ProductSalesTarget (DimProductID, DimTargetDateID,ProductTargetSalesQuantity)
 SELECT q2.PRODUCTID,q1.DATE_PKEY,q2.SALESQUANTITYTARGET FROM 
@@ -24,14 +23,57 @@ INNER JOIN
 STAGE_TARGETDATAPRODUCT q2 
 ON q1.FISCAL_YEAR = q2.YEAR;
 
+-- SELECT DATA FROM Fact_ProductSalesTarget
 
-Select * From Fact_ProductSalesTarget;
+SELECT * FROM Fact_ProductSalesTarget;
 
 
 /*
+ *  Fact_SRCSalesTarget
+ */
 
-*/
+-- UPDATE DIM_RESELLER SET RESELLERNAME='Mississippi Distributors' WHERE DIMRESELLERID=5;
+-- UPDATE DIM_CHANNEL SET CHANNELNAME='Online' WHERE DIMCHANNELID=3;
 
+
+-- DROP AND CREATE TABLE Fact_SRCSalesTarget
+
+DROP TABLE Fact_SRCSalesTarget;
+
+CREATE TABLE Fact_SRCSalesTarget(
+   DimStoreID INTEGER CONSTRAINT FK_DimStoreID FOREIGN KEY REFERENCES Dim_Store(DimStoreID), --Foreign Key
+   DimResellerID INTEGER CONSTRAINT FK_DimSResellerID FOREIGN KEY REFERENCES Dim_Reseller(DimResellerID), --Foreign Key
+   DimChannelID INTEGER CONSTRAINT FK_DimChannelID FOREIGN KEY REFERENCES Dim_Channel(DimChannelID),--Foreign Key
+   DimTargetDateID Number (9) CONSTRAINT FK_TargetDateID FOREIGN KEY REFERENCES Dim_Date(Date_Pkey), --Foreign Key
+   SalesTargetAmount FLOAT Not Null
+) 
+
+-- INSERT DATA INTO Fact_SRCSalesTarget
+
+INSERT INTO Fact_SRCSalesTarget(DimStoreID, DimResellerID,DimChannelID,DimTargetDateID,SalesTargetAmount)
+SELECT IFNULL(q5.DIMSTOREID,1) as DIMSTOREID, IFNULL(q4.DIMRESELLERID,1) as DIMRESELLERID, q3.DIMCHANNELID, q1.DATE_PKEY, q2.TARGETSALESAMOUNT
+From (SELECT FISCAL_YEAR, min(DATE_PKEY) as DATE_PKEY FROM DIM_DATE  GROUP BY FISCAL_YEAR) q1
+INNER JOIN
+STAGE_TARGETDATACHANNELRESELLERANDSTORE q2
+ON q1.FISCAL_YEAR = q2.YEAR
+INNER JOIN Dim_Channel q3
+ON q2.CHANNELNAME=q3.CHANNELNAME
+LEFT JOIN Dim_RESELLER q4
+ON q2.TARGETNAME=q4.RESELLERNAME
+LEFT JOIN Dim_STORE q5
+ON q2.TARGETNAME=CONCAT('Store Number ', q5.STORENUMBER)
+
+-- SELECT DATA FROM Fact_SRCSalesTarget
+
+SELECT * FROM Fact_SRCSalesTarget;
+
+/*
+ * Fact_SalesActual
+ */
+
+-- DROP AND CREATE TABLE Fact_SalesActual
+
+DROP TABLE Fact_SalesActual;
 
 CREATE TABLE Fact_SalesActual
 (
@@ -51,107 +93,28 @@ CREATE TABLE Fact_SalesActual
   SaleTotalProfit FLOAT NOT NULL
 );
 
+-- INSERT DATA INTO TABLE Fact_SalesActual
+INSERT INTO  Fact_SalesActual (DimProductID,DimStoreID,DimResellerID,DimCustomerID,DimChannelID,DimSaleDateID,DimLocationID,
+                               SourceSalesHeaderID,SourceSalesDetailID,SaleAmount,SaleQuantity,SaleUnitPrice,SaleExtendedCost,SaleTotalProfit)
+SELECT q3.PRODUCTID,IFNULL(q4.DIMSTOREID,1) as DIMSTOREID,IFNULL(q5.DIMRESELLERID, 1) as DIMRESELLERID, IFNULL(q6.DIMCUSTOMERID,1) as DIMCUSTOMERID,
+        IFNULL(q7.DIMCHANNELID,1) as DIMCHANNELID,q8.DATE_PKEY,IFNULL(q4.DIMLOCATIONID,IFNULL(q5.DIMLOCATIONID,q6.DIMLOCATIONID)) as DIMLOCATIONID,
+        q1.SALESHEADERID,q2.SALESDETAILID,q2.SALESAMOUNT,q2.SALESQUANTITY,q3.PRODUCTRETAILPRICE,q3.PRODUCTCOST,q3.PRODUCTRETAILPROFIT*q2.SALESQUANTITY
+FROM STAGESALESHEADER q1
+INNER JOIN STAGE_SALESDETAIL q2
+ON q1.SALESHEADERID=q2.SALESHEADERID
+INNER JOIN DIM_PRODUCT q3
+ON q2.PRODUCTID=q3.SOURCEPRODUCTID
+LEFT JOIN DIM_STORE q4
+ON q1.STOREID=q4.STOREID
+LEFT JOIN DIM_RESELLER q5
+ON q1.RESELLERID=q5.SOURCERESELLERID
+LEFT JOIN DIM_CUSTOMER q6
+ON q1.CUSTOMERID=q6.CUSTOMERID
+LEFT JOIN DIM_CHANNEL q7
+ON q1.CHANNELID=q7.SOURCECHANNELID
+INNER JOIN DIM_DATE q8
+ON q8.DATE=TO_DATE(q1.DATE,'MM/DD/YY');
 
 
-
-
- 
- Drop table Fact_SalesActual;
- 
- Select * From Fact_SalesActual;
- 
- INSERT INTO  Fact_SalesActual (DimProductID,DimStoreID,DimResellerID,DimCustomerID,DimChannelID,DimSaleDateID,DimLocationID,SourceSalesHeaderID,SourceSalesDetailID,SaleAmount,SaleQuantity,SaleUnitPrice,
-          SaleExtendedCost,SaleTotalProfit)
-              
-      Values  (-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1);
-      
-      INSERT INTO  Fact_SalesActual (DimProductID,DimStoreID,DimResellerID,DimCustomerID,DimChannelID,DimSaleDateID,DimLocationID,SourceSalesHeaderID,SourceSalesDetailID,SaleAmount,SaleQuantity,SaleUnitPrice,
-          SaleExtendedCost,SaleTotalProfit)
-          
-          Select Distinct DimProductID,DimStoreID,DimResellerID,DimCustomerID,DimChannelID,DimSaleDateID,DimLocationID,SourceSalesHeaderID,SourceSalesDetailID,SaleAmount,SaleQuantity,SaleUnitPrice,
-          SaleExtendedCost,SaleTotalProfit
-          
-  From Dim_Product
-  
-  
-  
-  
-  
-  Create Table Fact_SRCSalesTarget(
-    
-   DimStoreID INTEGER CONSTRAINT FK_DimStoreID FOREIGN KEY REFERENCES Dim_Store(DimStoreID), --Foreign Key
-   DimResellerID INTEGER CONSTRAINT FK_DimSResellerID FOREIGN KEY REFERENCES Dim_Reseller(DimResellerID), --Foreign Key
-   DimChannelID INTEGER CONSTRAINT FK_DimChannelID FOREIGN KEY REFERENCES Dim_Channel(DimChannelID),--Foreign Key
-   DimTargetDateID Number (9) CONSTRAINT FK_TargetDateID FOREIGN KEY REFERENCES Dim_Date(Date_Pkey), --Foreign Key
-   SalesTargetAmount FLOAT Not Null
-) 
-
-Drop table Fact_SRCSalesTarget;
-
-Select * From Fact_SRCSalesTarget
-
-INSERT INTO Fact_SRCSalesTarget(DimStoreID, DimResellerID,DimChannelID,DimTargetDateID, SalesTargetAmount)
-Values (-1,-1,-1,-1,-1);
-
----INSERt INTO Fact_SRCSalesTarget(DimStoreID, DimResellerID,DimChannelID,DimTargetDateID,SalesTargetAmount)
-Select q1.DATE_PKEY,q2.CHANNELNAME,q2.TARGETSALESAMOUNT
-From (SELECT FISCAL_YEAR, min(DATE_PKEY) as DATE_PKEY FROM DIM_DATE  GROUP BY FISCAL_YEAR) q1
-INNER JOIN
-STAGE_TARGETDATACHANNELRESELLERANDSTORE q2
-ON q1.FISCAL_YEAR = q2.YEAR
-INNER JOIN Dim_Channel  
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-//1/1/13 22:12
-//
-//SELECT CONVERT(date, CREATEDDATE) FROM STAGESALESHEADER LIMIT 1;
-//
-//
-//SELECT PRODUCTID,  TO_DATE ( DATE,'MM/DD/YY') AS TARGET_DATE, SALESQUANTITY FROM STAGE_SALESDETAIL;
-//
-//
-//
-//SELECT TO_DATE ( DATE,'MM/DD/YY') AS TARGET_DATE FROM STAGESALESHEADER LIMIT 1000;
-// 
-//
-//select to_date('2012-12-31 23:59:59','YYYY-MM-DD HH24:MI:SS') as DD
-//
-//
-//SELECT TOP 1000 * FROM STAGESALESHEADER;
-//
-//SELECT DATE_PKEY FROM DIM_DATE ORDER BY DATE_PKEY;
-
-
-
-
-
-SELECT TO_DATE ( DATE,'MM/DD/YY') AS TARGET_DATE FROM STAGESALESHEADER LIMIT 1000;
-    
-   
-          STAGE_SALESDETAIL
-
-SELECT DISTINCT YEAR FROM STAGE_TARGETDATAPRODUCT;
-
-
-INSERT Target_Fact
-SELECT (productid, salesquantityTarget, 1) FROM STAGE_TARGETDATAPRODUCT WHERE YEAR = 2013;
-
-
-INSERT INTO Fact_ProductSalesTarget (DimProductID, DimTargetDateID,ProductTargetSalesQuantity)
-SELECT q2.PRODUCTID,q1.DATE_PKEY,q2.SALESQUANTITYTARGET FROM 
-(SELECT FISCAL_YEAR, min(DATE_PKEY) as DATE_PKEY FROM DIM_DATE  GROUP BY FISCAL_YEAR) q1
-INNER JOIN
-(SELECT PRODUCTID, PRODUCT, YEAR, SALESQUANTITYTARGET FROM STAGE_TARGETDATAPRODUCT) q2
-ON q1.FISCAL_YEAR = q2.YEAR;
+-- SELECT DATA FROM Fact_SalesActual
+SELECT * FROM Fact_SalesActual;
